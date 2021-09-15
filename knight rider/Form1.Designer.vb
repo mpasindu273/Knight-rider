da49e99563d82573d6afc8f469b83d6be1ca155a
mpasindu273 <mpasindu273@gmail.com>
2021-09-15 08:08:08 +0530

    Knight rider light Signed-off-by: mpasindu273 <mpasindu273@gmail.com>

---
+++ knight rider/Form1.Designer.vb
@@ -0,0 +1,283 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.b1 = New System.Windows.Forms.Button()
+        Me.b2 = New System.Windows.Forms.Button()
+        Me.b3 = New System.Windows.Forms.Button()
+        Me.b4 = New System.Windows.Forms.Button()
+        Me.b5 = New System.Windows.Forms.Button()
+        Me.b6 = New System.Windows.Forms.Button()
+        Me.b7 = New System.Windows.Forms.Button()
+        Me.b8 = New System.Windows.Forms.Button()
+        Me.b9 = New System.Windows.Forms.Button()
+        Me.b10 = New System.Windows.Forms.Button()
+        Me.b11 = New System.Windows.Forms.Button()
+        Me.b12 = New System.Windows.Forms.Button()
+        Me.b13 = New System.Windows.Forms.Button()
+        Me.b14 = New System.Windows.Forms.Button()
+        Me.b15 = New System.Windows.Forms.Button()
+        Me.b16 = New System.Windows.Forms.Button()
+        Me.b17 = New System.Windows.Forms.Button()
+        Me.b18 = New System.Windows.Forms.Button()
+        Me.b19 = New System.Windows.Forms.Button()
+        Me.b20 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.tim = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'b1
+        '
+        Me.b1.BackColor = System.Drawing.Color.White
+        Me.b1.Location = New System.Drawing.Point(12, 100)
+        Me.b1.Name = "b1"
+        Me.b1.Size = New System.Drawing.Size(16, 16)
+        Me.b1.TabIndex = 0
+        Me.b1.UseVisualStyleBackColor = False
+        '
+        'b2
+        '
+        Me.b2.BackColor = System.Drawing.Color.White
+        Me.b2.Location = New System.Drawing.Point(34, 100)
+        Me.b2.Name = "b2"
+        Me.b2.Size = New System.Drawing.Size(16, 16)
+        Me.b2.TabIndex = 1
+        Me.b2.UseVisualStyleBackColor = False
+        '
+        'b3
+        '
+        Me.b3.BackColor = System.Drawing.Color.White
+        Me.b3.Location = New System.Drawing.Point(56, 100)
+        Me.b3.Name = "b3"
+        Me.b3.Size = New System.Drawing.Size(16, 16)
+        Me.b3.TabIndex = 2
+        Me.b3.UseVisualStyleBackColor = False
+        '
+        'b4
+        '
+        Me.b4.Location = New System.Drawing.Point(78, 100)
+        Me.b4.Name = "b4"
+        Me.b4.Size = New System.Drawing.Size(16, 16)
+        Me.b4.TabIndex = 3
+        Me.b4.UseVisualStyleBackColor = True
+        '
+        'b5
+        '
+        Me.b5.Location = New System.Drawing.Point(100, 100)
+        Me.b5.Name = "b5"
+        Me.b5.Size = New System.Drawing.Size(16, 16)
+        Me.b5.TabIndex = 4
+        Me.b5.UseVisualStyleBackColor = True
+        '
+        'b6
+        '
+        Me.b6.Location = New System.Drawing.Point(122, 100)
+        Me.b6.Name = "b6"
+        Me.b6.Size = New System.Drawing.Size(16, 16)
+        Me.b6.TabIndex = 5
+        Me.b6.UseVisualStyleBackColor = True
+        '
+        'b7
+        '
+        Me.b7.Location = New System.Drawing.Point(144, 100)
+        Me.b7.Name = "b7"
+        Me.b7.Size = New System.Drawing.Size(16, 16)
+        Me.b7.TabIndex = 6
+        Me.b7.UseVisualStyleBackColor = True
+        '
+        'b8
+        '
+        Me.b8.Location = New System.Drawing.Point(166, 100)
+        Me.b8.Name = "b8"
+        Me.b8.Size = New System.Drawing.Size(16, 16)
+        Me.b8.TabIndex = 7
+        Me.b8.UseVisualStyleBackColor = True
+        '
+        'b9
+        '
+        Me.b9.Location = New System.Drawing.Point(188, 100)
+        Me.b9.Name = "b9"
+        Me.b9.Size = New System.Drawing.Size(16, 16)
+        Me.b9.TabIndex = 8
+        Me.b9.UseVisualStyleBackColor = True
+        '
+        'b10
+        '
+        Me.b10.Location = New System.Drawing.Point(209, 100)
+        Me.b10.Name = "b10"
+        Me.b10.Size = New System.Drawing.Size(16, 16)
+        Me.b10.TabIndex = 9
+        Me.b10.UseVisualStyleBackColor = True
+        '
+        'b11
+        '
+        Me.b11.Location = New System.Drawing.Point(231, 100)
+        Me.b11.Name = "b11"
+        Me.b11.Size = New System.Drawing.Size(16, 16)
+        Me.b11.TabIndex = 10
+        Me.b11.UseVisualStyleBackColor = True
+        '
+        'b12
+        '
+        Me.b12.Location = New System.Drawing.Point(253, 100)
+        Me.b12.Name = "b12"
+        Me.b12.Size = New System.Drawing.Size(16, 16)
+        Me.b12.TabIndex = 11
+        Me.b12.UseVisualStyleBackColor = True
+        '
+        'b13
+        '
+        Me.b13.Location = New System.Drawing.Point(275, 100)
+        Me.b13.Name = "b13"
+        Me.b13.Size = New System.Drawing.Size(16, 16)
+        Me.b13.TabIndex = 12
+        Me.b13.UseVisualStyleBackColor = True
+        '
+        'b14
+        '
+        Me.b14.Location = New System.Drawing.Point(297, 100)
+        Me.b14.Name = "b14"
+        Me.b14.Size = New System.Drawing.Size(16, 16)
+        Me.b14.TabIndex = 13
+        Me.b14.UseVisualStyleBackColor = True
+        '
+        'b15
+        '
+        Me.b15.Location = New System.Drawing.Point(319, 100)
+        Me.b15.Name = "b15"
+        Me.b15.Size = New System.Drawing.Size(16, 16)
+        Me.b15.TabIndex = 14
+        Me.b15.UseVisualStyleBackColor = True
+        '
+        'b16
+        '
+        Me.b16.Location = New System.Drawing.Point(341, 100)
+        Me.b16.Name = "b16"
+        Me.b16.Size = New System.Drawing.Size(16, 16)
+        Me.b16.TabIndex = 15
+        Me.b16.UseVisualStyleBackColor = True
+        '
+        'b17
+        '
+        Me.b17.Location = New System.Drawing.Point(363, 100)
+        Me.b17.Name = "b17"
+        Me.b17.Size = New System.Drawing.Size(16, 16)
+        Me.b17.TabIndex = 16
+        Me.b17.UseVisualStyleBackColor = True
+        '
+        'b18
+        '
+        Me.b18.Location = New System.Drawing.Point(385, 100)
+        Me.b18.Name = "b18"
+        Me.b18.Size = New System.Drawing.Size(16, 16)
+        Me.b18.TabIndex = 17
+        Me.b18.UseVisualStyleBackColor = True
+        '
+        'b19
+        '
+        Me.b19.Location = New System.Drawing.Point(406, 100)
+        Me.b19.Name = "b19"
+        Me.b19.Size = New System.Drawing.Size(16, 16)
+        Me.b19.TabIndex = 18
+        Me.b19.UseVisualStyleBackColor = True
+        '
+        'b20
+        '
+        Me.b20.Location = New System.Drawing.Point(428, 100)
+        Me.b20.Name = "b20"
+        Me.b20.Size = New System.Drawing.Size(16, 16)
+        Me.b20.TabIndex = 19
+        Me.b20.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(188, 161)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 33)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'tim
+        '
+        Me.tim.Enabled = True
+        Me.tim.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(449, 237)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.b20)
+        Me.Controls.Add(Me.b19)
+        Me.Controls.Add(Me.b18)
+        Me.Controls.Add(Me.b17)
+        Me.Controls.Add(Me.b16)
+        Me.Controls.Add(Me.b15)
+        Me.Controls.Add(Me.b14)
+        Me.Controls.Add(Me.b13)
+        Me.Controls.Add(Me.b12)
+        Me.Controls.Add(Me.b11)
+        Me.Controls.Add(Me.b10)
+        Me.Controls.Add(Me.b9)
+        Me.Controls.Add(Me.b8)
+        Me.Controls.Add(Me.b7)
+        Me.Controls.Add(Me.b6)
+        Me.Controls.Add(Me.b5)
+        Me.Controls.Add(Me.b4)
+        Me.Controls.Add(Me.b3)
+        Me.Controls.Add(Me.b2)
+        Me.Controls.Add(Me.b1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents b1 As System.Windows.Forms.Button
+    Friend WithEvents b2 As System.Windows.Forms.Button
+    Friend WithEvents b3 As System.Windows.Forms.Button
+    Friend WithEvents b4 As System.Windows.Forms.Button
+    Friend WithEvents b5 As System.Windows.Forms.Button
+    Friend WithEvents b6 As System.Windows.Forms.Button
+    Friend WithEvents b7 As System.Windows.Forms.Button
+    Friend WithEvents b8 As System.Windows.Forms.Button
+    Friend WithEvents b9 As System.Windows.Forms.Button
+    Friend WithEvents b10 As System.Windows.Forms.Button
+    Friend WithEvents b11 As System.Windows.Forms.Button
+    Friend WithEvents b12 As System.Windows.Forms.Button
+    Friend WithEvents b13 As System.Windows.Forms.Button
+    Friend WithEvents b14 As System.Windows.Forms.Button
+    Friend WithEvents b15 As System.Windows.Forms.Button
+    Friend WithEvents b16 As System.Windows.Forms.Button
+    Friend WithEvents b17 As System.Windows.Forms.Button
+    Friend WithEvents b18 As System.Windows.Forms.Button
+    Friend WithEvents b19 As System.Windows.Forms.Button
+    Friend WithEvents b20 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents tim As System.Windows.Forms.Timer
+
+End Class
---
+++ knight rider/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Dim i As Integer
+    Dim x As Integer = 1
+
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim b As Button() = {b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20}
+       
+        x = x + 1
+
+        If x Mod 2 = 0 Then
+            Button1.Text = "Stop"
+
+        Else
+            Button1.Text = "Start"
+        End If
+
+        While x Mod 2 = 0
+
+            Dim decre As Integer = 10
+            Dim incre As Integer = 1
+
+            For Me.i = 0 To 19
+
+                b(i).BackColor = Color.Red
+
+                If i > 9 Then
+                    b(i - 10).BackColor = Color.White
+                End If
+                Await Task.Delay(25)
+
+            Next i
+
+
+            While decre > 1
+                b(i - decre).BackColor = Color.White
+                decre -= 1
+                Await Task.Delay(60)
+            End While
+
+            i = 19
+            For Me.i = 19 To 0 Step -1
+
+                b(i).BackColor = Color.Red
+
+
+                If i < 11 Then
+                    b(i + 9).BackColor = Color.White
+                End If
+                Await Task.Delay(25)
+
+            Next i
+
+            i = 9
+            While incre < 9
+                b(i - incre).BackColor = Color.White
+                incre += 1
+                Await Task.Delay(60)
+            End While
+
+
+        End While
+    End Sub
+
+    
+  
+   
+    
+End Class
